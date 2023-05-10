719345ee30c75bc354b0a7ab633015267b78e2de
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-09 21:49:04 -0300

    Conheçendo JSX

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -1,4 +1,11 @@
 import ReactDOM  from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
-ReactDOM.render('Olá React!!!', el)
+const tag = <strong>Olá React!</strong>
+
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    document.getElementById('root')
+)
